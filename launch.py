ce9f476a8e05c2222b9ac9100eebb1f94d1b21b9
Jake Stephens <sjakedude@gmail.com>
2021-07-14 09:27:58 -0400

    refactor

--- launch.py
+++ launch.py
@@ -108,73 +108,78 @@ for k in range(1, 4):
     num_pos, num_neg = get_positive_probability(training_set, k)
     print("======================")
     print("For k=" + str(k))
-    print("Probability = " + str(num_pos) + ":" + str(num_neg))
+    print("Probability (pos:neg) = " + str(num_pos) + ":" + str(num_neg))
     chance_up[k]["Pos"] = num_pos
     chance_up[k]["Neg"] = num_neg
 
 # =================
 # Predicting Labels
 # =================
-print("DOWN")
-print(chance_down)
+print("=================")
+print("Predicting Labels")
+print("=================")
 
-print("UP")
-print(chance_up)
 
 # Testing set of last 2 years
 testing_set = df.loc[df["Year"] > 2018]
 testing_set["True Label"] = testing_set.apply(lambda row: true_label(row), axis=1)
 
-w = 3
-d = 1
+def predict_next_label(w, df):
+    
+    d = 1
+    index = df.index[0]
+    num_correct = 0
+    num_incorrect = 0
 
-d_plus_one = None
+    while index < len(df.index) + df.index[0]:
+        # preventing key error from viewing indexs outside dataset (in beginning 3)
+        if d < w:
+            d = d + 1
+            index = index + 1
+        else:
+            # generating sequence of last 3 labels, including current day d
+            s = [None] * w
+            for i in range(w):
+                s[i] = df.loc[index - i]["True Label"]
+            index = index + 1
+            symbol = s[-1]
+            num_consecutive = 1
+            prediction = None
+            # calculating the number of consecutive labels
+            for i in range(1, w):
+                if s[-1 - i] == symbol and num_consecutive == i:
+                    num_consecutive = num_consecutive + 1
+            if symbol == "-":
+                # compare to chance_down
+                if chance_down[num_consecutive]["Pos"] >= chance_down[num_consecutive]["Neg"]:
+                    prediction = "+"
+                elif chance_down[num_consecutive]["Pos"] < chance_down[num_consecutive]["Neg"]:
+                    prediction = "-"
+            elif symbol == "+":
+                # compare to chance_up
+                if chance_up[num_consecutive]["Pos"] >= chance_up[num_consecutive]["Neg"]:
+                    prediction = "+"
+                elif chance_up[num_consecutive]["Pos"] < chance_up[num_consecutive]["Neg"]:
+                    prediction = "-"
+            # Preventing key error from trying to predict the next label outside the dataset
+            if index + 1 < len(df.index) + df.index[0]:
+                if prediction == df.loc[index + 1]["True Label"]:
+                    num_correct = num_correct + 1
+                else:
+                    num_incorrect = num_incorrect + 1
+    print("Accuracy = " + str(round((num_correct / len(df)) * 100, 2)) + "%")
 
-s = []
 
-index = testing_set.index[0]
-num_correct = 0
-num_incorrect = 0
+print("For SUN with w=3")
+predict_next_label(w=3, df=testing_set)
+print("------------------")
 
-while index < len(testing_set.index) + testing_set.index[0]:
-    # preventing key error from viewing indexs outside dataset (in beginning 3)
-    if d < w:
-        d = d + 1
-        index = index + 1
-    else:
-        # generating sequence of last 3 labels, including current day d
-        s = [
-            testing_set.loc[index]["True Label"],
-            testing_set.loc[index - 1]["True Label"],
-            testing_set.loc[index - 2]["True Label"],
-        ]
-        index = index + 1
-        symbol = s[-1]
-        num_consecutive = 1
-        prediction = None
-        # calculating the number of consecutive labels
-        if s[-2] == symbol:
-            num_consecutive = 2
-            if s[-3] == symbol:
-                num_consecutive = 3
-        if symbol == "-":
-            # compare to chance_down
-            if chance_down[num_consecutive]["Pos"] >= chance_down[num_consecutive]["Neg"]:
-                prediction = "+"
-            elif chance_down[num_consecutive]["Pos"] < chance_down[num_consecutive]["Neg"]:
-                prediction = "-"
-        elif symbol == "+":
-            # compare to chance_up
-            if chance_up[num_consecutive]["Pos"] >= chance_up[num_consecutive]["Neg"]:
-                prediction = "+"
-            elif chance_up[num_consecutive]["Pos"] < chance_up[num_consecutive]["Neg"]:
-                prediction = "-"
-        # Preventing key error from trying to predict the next label outside the dataset
-        if index + 1 < len(testing_set.index) + testing_set.index[0]:
-            if prediction == testing_set.loc[index + 1]["True Label"]:
-                num_correct = num_correct + 1
-            else:
-                num_incorrect = num_incorrect + 1
+print("For SUN with w=2")
+predict_next_label(w=2, df=testing_set)
+print("------------------")
 
-print(num_correct)
-print(num_incorrect)
+print("For SUN with w=1")
+predict_next_label(w=1, df=testing_set)
+print("------------------")
+
+
